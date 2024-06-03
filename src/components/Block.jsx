5e69395eb6f294d0001a8d279c9b0eef6fa3ba20
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-06-03 16:24:39 -0300

    :art: add block status

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -9,20 +9,26 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { Button } from './ui/button'
+import { CheckCircledIcon, CrossCircledIcon } from "@radix-ui/react-icons"
 
 const Block = () => {
     return (
         <Card>
-            <CardHeader>
-                <CardTitle>Block</CardTitle>
-                <CardDescription>Block Number 1</CardDescription>
+            <CardHeader className='flex-row justify-between'>
+                <div>
+                    <CardTitle>Block</CardTitle>
+                    <CardDescription>Block Number 1</CardDescription>
+                </div>
+                <CheckCircledIcon className='w-6 h-6 text-constructive'/>
+                {/* <CrossCircledIcon className='w-6 h-6 text-destructive'/> */}
+                
             </CardHeader>
             <CardContent className='flex flex-col gap-4'>
                 <InputGroup label='Nonce:' id='nonce' type='text' />
                 <InputGroup label='Data:' id='data' type='text' />
-                <InputGroup label='Previous Hash:' id='previous-hash' type='text' disabled/>
-                <InputGroup label='Hash:' id='hash' type='text' disabled/>
-            </CardContent>  
+                <InputGroup label='Previous Hash:' id='previous-hash' type='text' disabled />
+                <InputGroup label='Hash:' id='hash' type='text' disabled />
+            </CardContent>
             <CardFooter>
                 <Button className='ml-auto'>Mine</Button>
             </CardFooter>
